494cfc0d17556fbe4c3a127aa840d271056cb8ca
NitinSinghElegant <130339696+NitinSinghElegant@users.noreply.github.com>
2024-12-31 23:03:14 +0530

    add base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 import { AUTH_TOKEN, USER } from "@/constants/auth";
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
+const baseURL = "https://expocon.webtimate.com/api/v1";
+//process.env.NEXT_PUBLIC_API_URL;
 
 export const axiosInstance = axios.create({
   baseURL,

8aa26c1d79b4e824b986fb8b93b27e29acb92caf
NitinSinghElegant <130339696+NitinSinghElegant@users.noreply.github.com>
2025-01-02 23:29:52 +0530

    update not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,10 +4,10 @@ import { ArrowLeft } from "lucide-react";
 
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+    <div className="flex items-center justify-center min-h-screen bg-primary from-purple-400 via-pink-500 to-red-500">
       <div className="text-center text-white px-4">
         <h1 className="text-6xl font-bold mb-4">404</h1>
-        <p className="text-3xl mb-8">Oops! Page not found</p>
+        <p className="text-3xl mb-6">Oops! Page not found</p>
         <p className="text-xl mb-8">
           The page you are looking for does not exist or has been moved.
         </p>
@@ -25,7 +25,7 @@ export default function NotFound() {
           <Button
             asChild
             variant="outline"
-            className="bg-white text-purple-600 hover:bg-purple-100"
+            className=" text-black hover:bg-white bg-white"
           >
             <Link href="javascript:history.back()">
               <ArrowLeft className="mr-2 h-4 w-4" />
